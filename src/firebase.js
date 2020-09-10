0db991152fc8a777ae9dd6b0bbac41127d522526
Anvesh-Chinta <pratanvesh@gmail.com>
2020-09-10 01:51:26 -0500

    First React App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAwNDP6lTIPlXA4xUbQJDts45QfQGnkB14',
+  authDomain: 'zona-87983.firebaseapp.com',
+  databaseURL: 'https://zona-87983.firebaseio.com',
+  projectId: 'zona-87983',
+  storageBucket: 'zona-87983.appspot.com',
+  messagingSenderId: '87888204090',
+  appId: '1:87888204090:web:18a7372588222564708003',
+  measurementId: 'G-BF9RQ5C38M',
+};
