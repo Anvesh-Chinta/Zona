bfc6fc1e177e7b3f8d8421e16af885395ed75346
Anvesh-Chinta <pratanvesh@gmail.com>
2020-09-13 01:00:37 -0500

    Initial react-router-dom, checkout page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import Home from './Home.js';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     // BEM
-    <div className='app'>
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className='app'>
+        <Switch>
+          <Route path='/checkout'>
+            <h1>checkout page here</h1>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+     </Router>
   );
 }
 
